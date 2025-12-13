5ba41a4ff9e824e021620ddcd1cfd01a1b398f0f
st-code-git <lehner.st@gmail.com>
2025-12-13 12:45:41 +0100

    Built web
    Branch: main
    Commit: d688de09be08a9cc5622738023a0058d0f3b59ad (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1807129307"
+    serviceWorkerVersion: "1312072223"
   }
 });
